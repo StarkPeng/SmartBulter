278504a5016102f059d5eded4ef5846a7b652b58
starkpeng <67726711@qq.com>
2017-03-20 16:10:06 +0800

    2017.3.20

--- app/src/main/java/com/stark/smartbutler/fragment/WechatFragment.java
+++ app/src/main/java/com/stark/smartbutler/fragment/WechatFragment.java
@@ -3,6 +3,8 @@ package com.stark.smartbutler.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,9 +39,13 @@ import java.util.List;
 public class WechatFragment extends Fragment {
 
     private ListView mListView;
+    private SwipeRefreshLayout mListRefresh;
     private List<WeChatData> mList = new ArrayList<>();
     private List<String> mListTitle = new ArrayList<>();
     private List<String> mListUrl = new ArrayList<>();
+    private WeChatAdapter adapter;
+    private int flag = 0;
+    private int ps = 10;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_wechat,null);
@@ -49,9 +55,9 @@ public class WechatFragment extends Fragment {
     //初始化view
     private void findView(View view) {
         mListView = (ListView) view.findViewById(R.id.mListView);
-
+        mListRefresh = (SwipeRefreshLayout) view.findViewById(R.id.mListRefresh);
         //解析接口
-        String url = "http://v.juhe.cn/weixin/query?key=" + StaticClass.WECHAT_KEY +"&ps=50";
+        String url = "http://v.juhe.cn/weixin/query?key=" + StaticClass.WECHAT_KEY +"&ps=10";
         RxVolley.get(url, new HttpCallback() {
             @Override
             public void onSuccess(String t) {
@@ -72,7 +78,74 @@ public class WechatFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        mListRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //解析接口
+                if (ps == 50) {
+                    ps =10;
+                }
+                String url = "http://v.juhe.cn/weixin/query?key=" + StaticClass.WECHAT_KEY +"&ps=" +ps ;
+                ps = ps +5;
+                RxVolley.get(url, new HttpCallback() {
+                    @Override
+                    public void onSuccess(String t) {
+                        //Toast.makeText(getActivity(),t,Toast.LENGTH_SHORT).show();
+                        //super.onSuccess(t);
+                        //解析JSON
+                        updateJson(t);
+                    }
+
+
+                });
+                //关闭刷新动画
+                mListRefresh.setRefreshing(false);
+            }
+
+
+        });
     }
+
+    private void updateJson(String t) {
+        try {
+            JSONObject jsonObject = new JSONObject(t);
+            JSONObject jsonResult = jsonObject.getJSONObject("result");
+            JSONArray jsonList = jsonResult.getJSONArray("list");
+            for (int i = 0 ;i < jsonList.length() ;i++) {
+                JSONObject json = (JSONObject) jsonList.get(i);
+                WeChatData data = new WeChatData();
+
+                String title = json.getString("title");
+                String url = json.getString("url");
+
+                data.setTitle(title);
+                data.setSource(json.getString("source"));
+                data.setImgUrl(json.getString("firstImg"));
+               // L.i("firstImg : "+ json.getString("firstImg"));
+                L.i("title" + title);
+                flag = 0;
+                for(int j = 0;j<mList.size();j++){
+                   // L.i("mList :" + mList.get(j).getTitle());
+                    if (mList.get(j).getTitle().equals(title)) {
+                        flag++ ;
+                        L.i("flag :" + flag);
+                    }
+                }
+                if (flag == 0) {
+                    mList.add(0,data);
+                    mListTitle.add(0,title);
+                    mListUrl.add(0,url);
+                    adapter.notifyDataSetChanged();
+
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     private void parsingJson(String t) {
         try {
             JSONObject jsonObject = new JSONObject(t);
@@ -94,7 +167,7 @@ public class WechatFragment extends Fragment {
                 mListTitle.add(title);
                 mListUrl.add(url);
             }
-            WeChatAdapter adapter = new WeChatAdapter(getActivity(),mList);
+            adapter = new WeChatAdapter(getActivity(),mList);
             mListView.setAdapter(adapter);
         } catch (JSONException e) {
             e.printStackTrace();
